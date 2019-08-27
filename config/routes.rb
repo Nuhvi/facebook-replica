4821a74b958fbd216395a3737b7e5f2a0d2ef620
themonster2015 <quynhyen.vothi@gmail.com>
2019-08-27 16:38:18 +0200

    added userscontroller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/show'
   devise_for :users
+  root 'users#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
