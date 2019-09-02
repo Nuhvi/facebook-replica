538ab555565ab2bcae886141af78e7c1ad48c165
themonster2015 <quynhyen.vothi@gmail.com>
2019-09-02 15:58:57 +0200

    user show all posts in view

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,16 +3,11 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
-  describe 'GET #index' do
-    it 'returns http success' do
-      get :index
-      expect(response).to be_successful
-    end
-  end
 
-  describe 'GET #show' do
-    it 'returns http success' do
-      get :show, params: { id: 1 }
+  describe '#show' do
+  before {@user = FactoryBot.create(:user)}
+    it 'responds successfully' do
+      get :show, params: { id: @user.id }
       expect(response).to be_successful
     end
   end
