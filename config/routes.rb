94fa7aeb51f594731f1604ab32ec5c17df2adc39
Nazeh <ar.nazeh@gmail.com>
2019-09-02 13:21:14 +0300

    users model unit tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'users', to: 'users#index'
-  get 'users/show', to: 'users#show'
   devise_for :users
+  resources :users, only: [:index, :show]
   root 'users#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,25 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'has a valid factory' do
+    expect(FactoryBot.build(:user)).to be_valid
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:email) }
+    it { should validate_uniqueness_of(:email).case_insensitive }
+    it { should validate_presence_of(:password) }
+    it { should validate_length_of(:password).is_at_least(6) }
+    it { should validate_presence_of(:first_name) }
+    it { should validate_presence_of(:last_name) }
+  end
+
+  describe 'associations' do
+    xit { should have_many(:posts) }
+  end
+
+  describe 'methods' do
+  end
 end
