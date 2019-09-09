eb5f8db45f957dfa142e5267b9e4583d8ee34dfb
Nazeh <ar.nazeh@gmail.com>
2019-09-09 16:16:12 +0300

    some UI customization

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,9 +1,13 @@
 # frozen_string_literal: true
 
 module UsersHelper
-  def gravatar_for(user)
+  def avatar_for(user)
     gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
-    image_tag(gravatar_url, alt: user.first_name, class: 'gravatar')
+    image_tag(gravatar_url, alt: user.first_name, class: 'avatar')
+  end
+
+  def name_for(user)
+    "<p class = 'name'> #{user.first_name}</p>"
   end
 end
