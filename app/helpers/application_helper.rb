d652f87cabda1d8aa78fc04e7a776d25b4ef5642
Nazeh <ar.nazeh@gmail.com>
2019-09-09 19:13:10 +0300

    more UI customization

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,12 @@ module ApplicationHelper
     base_title = 'Fakebook'
     page_title.empty? ? base_title : page_title + ' | ' + base_title
   end
+
+  def alert_type(message_type)
+    if message_type == 'notice'
+      return 'success'
+    elsif message_type == 'alert' 
+      return 'danger'
+    end
+  end
 end
