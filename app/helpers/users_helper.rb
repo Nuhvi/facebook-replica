1c97cdbfd8877a510dd1a22116edb68423db3dbe
Nazeh <ar.nazeh@gmail.com>
2019-09-10 14:00:48 +0300

    another changes to UI

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -4,10 +4,11 @@ module UsersHelper
   def avatar_for(user)
     gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
-    image_tag(gravatar_url, alt: user.first_name, class: 'avatar')
+    image = image_tag(gravatar_url, alt: user.first_name)
+    ("<a href='#{user_path(user)}' class='avatar'>#{image}</a>").html_safe
   end
 
   def name_for(user)
-    "<p class = 'name'> #{user.first_name}</p>"
+    ("<a href='#{user_path(user)}' class='username'> #{user.first_name + " " + user.last_name} </a>").html_safe
   end
 end
