cb6aab1f85dee0a741730af74318527f343a77d2
Nazeh <ar.nazeh@gmail.com>
2019-09-19 17:04:58 +0300

    Omniauth allow sign in and sign out w/ FB

--- app/models/user.rb
+++ app/models/user.rb
@@ -30,6 +30,18 @@ class User < ApplicationRecord
            through: :friendships,
            source: :friend
 
+  # OmniAuth
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0, 20]
+      user.first_name = auth.info.first_name
+      user.last_name = auth.info.last_name
+      user.profile_image = auth.info.image
+    end
+  end
+
   # Friendships methods
 
   def strangers
